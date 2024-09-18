f3766f7d2576a41c214661e5c5b099b8071e8006
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2024-09-18 03:43:31 +0000

    Added form

--- webapps/icecream-app/WEB-INF/src/HomeServlet.java
+++ webapps/icecream-app/WEB-INF/src/HomeServlet.java
@@ -15,7 +15,44 @@ public class HomeServlet extends HttpServlet {
         out.println("<head><title>My Icecream Shop!</title></head>");
         out.println("<body>");
         out.println("<h1>Welcome To My Ice Cream Parlor</h1>");
+        out.println("<a href='/icecream-app/form.html'>Place An Order!</a>"); // link to form
         out.println("</body></html>");
       }
+
+    //   public void doPost(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException {
+    //     String cupOrCone = request.getParameter("cupOrCone");
+    //     String size = request.getParameter("size");
+    //     String flavor = request.getParameter("flavor");
+              
+    //     PrintWriter out = response.getWriter();   // Get the response PrintWriter
+    //     out.println("<html>");
+    //     out.println("<head><title>My Icecream Shop!</title></head>");
+    //     out.println("<body>");
+    //     out.println(String.format("<h1>Here is your %s %s ice cream in a %s. Enjoy!</h1>", size, flavor, cupOrCone));
+    //     out.println("</body></html>");
+    //   }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException {
+        String cupOrCone = request.getParameter("cupOrCone");
+        String size = request.getParameter("size");
+        String flavor = request.getParameter("flavor");
+        HttpSession session = request.getSession(true);  // Get or create session object
+      
+        String prevSize = (String)session.getAttribute("size");  // Get previously saved size value
+        String prevFlavor = (String)session.getAttribute("flavor");  // Get previously saved flavor value
+        String prevCupOrCone = (String)session.getAttribute("cupOrCone");  // Get previously saved cupOrCone value
+      
+        session.setAttribute("size", size);  // Update size option
+        session.setAttribute("flavor", flavor);  // Update flavor option
+        session.setAttribute("cupOrCone", cupOrCone);  // Update cupOrCone option
+
+        PrintWriter out = response.getWriter();   // Get the response PrintWriter
+        out.println("<html>");
+        out.println("<head><title>My Icecream Shop!</title></head>");
+        out.println("<body>");
+        out.println(String.format("<h1>Here is your %s %s ice cream in a %s. Enjoy!</h1>", size, flavor, cupOrCone));
+        out.println("</body></html>");        
+      }    
+      
       
   }
