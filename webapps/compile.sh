b53ace626978cd293f4b8c1b2289ee865af82a50
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2024-09-19 10:24:27 +0000

    Removed warning

--- webapps/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-javac -cp /usr/local/tomcat/lib/servlet-api.jar -d icecream-app/WEB-INF/classes/ icecream-app/WEB-INF/src/HomeServlet.java
---
+++ webapps/icecream-app/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javac -cp /usr/local/tomcat/lib/servlet-api.jar -d WEB-INF/classes/ WEB-INF/src/HomeServlet.java
