ce3993e48589a97af85b86574277ae1f407094a6
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2024-09-21 16:31:06 +0000

    JNI example working

--- jni1/SubtractionFun.java
+++ jni1/SubtractionFun.java
@@ -1,13 +1,15 @@
 public class SubtractionFun {
     static {
-      System.loadLibrary("native");
+      System.loadLibrary("SubtractionFun");
     }
   
     private native int subtractValues(double num1, double num2);
   
     public static void main(String[] args) {
-      double num1 = 1;
-      double num2 = 2;
+      double num1 = 1.3;
+      double num2 = 2.5;
       int difference = new SubtractionFun().subtractValues(num1, num2);
+
+      System.out.println("difference: " + difference);
     }
   }
--- jni1/compile.sh
+++ jni1/compile.sh
@@ -1,3 +1,2 @@
 
 javac -h . SubtractionFun.java
-gcc -I/usr/lib/jvm/java-8-oracle/include -I/usr/lib/jvm/java-8-oracle/include/linux -I/home/user1/JNI_project/ -o /home/user1/JNI_project/mynativelib.so /home/user1/JNI_project/FindSum.cpp
